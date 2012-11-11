f89e3d613702742365eae3340f2fc92be8dea90c
Jeremy Seago <seagoj@gmail.com>
2012-11-10 20:10:15 -0600

    Configure recipe to deploy php5-fpm

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,18 +1,6 @@
 # --- Install packages we need ---
-package 'php5-cgi'
+package 'php5-fpm'
 
-# --- Deploy a configuration file ---
-# For longer files, when using 'content "..."' becomes too
-# cumbersome, we can resort to deploying separate files:
-cookbook_file '/etc/init.d/php-fastcgi' do
-  source "php-fastcgi.d"
-  mode "0777"
-end
-
-execute "start-php-fastcgi" do
-  command "/etc/init.d/php-fastcgi start"
-end
-
-execute "install-php-as-service" do
-  command "update-rc.d php-fastcgi defaults"
+execute "start-php5-fpm" do
+  command "/etc/init.d/php5-fpm start"
 end
