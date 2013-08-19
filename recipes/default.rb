b873e2b5ce804a7200ec7999d1822ad627805fcc
Jeremy Seago <seagoj@gmail.com>
2013-08-19 15:01:55 -0500

    Performs phpunit installation

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,13 @@ package 'php5-cli'
 package 'php-pear'
 package 'phpunit'
 
+execute "phpunit" do
+    command "pear upgrade pear"
+    command "pear channel-discover pear.phpunit.de"
+    command "pear channel-discover components.ez.no"
+    command "pear install --alldeps phpunit/PHPUnit"
+end
+
 execute "start-php5-fpm" do
   command "/etc/init.d/php5-fpm start"
 end
