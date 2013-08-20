ebcc0d1e4a7f7edafee05fdd1fca26a82c176cbd
Jeremy Seago <seagoj@gmail.com>
2013-08-20 09:35:01 -0500

    Adds xdebug configuration

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,5 @@
 # --- Install packages we need ---
+package 'make'
 package 'php5-fpm'
 package 'php5'
 package 'php5-cli'
@@ -9,6 +10,14 @@ execute "phpunit" do
     command "sudo pear upgrade pear && pear channel-discover pear.phpunit.de && sudo pear channel-discover components.ez.no && sudo pear channel-discover pear.symfony.com && sudo pear install --alldeps phpunit/PHPUnit"
 end
 
-execute "start-php5-fpm" do
-  command "/etc/init.d/php5-fpm start"
+execute "xdebug" do
+    command "sudo pecl install xdebug"
 end
+
+cookbook_file "/etc/php5/cli/php.ini" do
+    source "xdebug-php.ini"
+    mode "0777"
+end
+
+execute "start-php5-fpm" do
+  command "/etc/init.d/php5-fpm start"end
