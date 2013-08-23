f973efcc2f1f8cc975253ed7512da8380b4acb3f
Jeremy Seago <seagoj@gmail.com>
2013-08-22 19:07:09 -0500

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name              "php"
+description       "Installs and configures PHP"
+version           "0.1"
+depends           "apt"
+depends           "nginx"
+
+recipe "php", "Installs and configures a PHP development environment."
