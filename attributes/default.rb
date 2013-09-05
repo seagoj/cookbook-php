a394d38c3cffd9775f3b3c0b347689039ec3b1da
Jeremy Seago <seagoj@gmail.com>
2013-09-04 20:26:43 -0500

    Modifies recipe to use attributes

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default[:php][:user]        = "root"
+default[:php][:group]       = "root"
+default[:php][:source_dir]  = "/usr/src"
+default[:php][:config_dir]  = "/etc"
+default[:php][:log_dir]     = "/var/log"
+default[:php][:deb_file]    = "php_5.5.2-1_amd64.deb"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,65 +1,49 @@
-# --- Install packages we need ---
 %w(make autoconf bison libxml2 libxml2-dev libcurl3 libcurl4-gnutls-dev libmagic-dev curl).each do |p|
     package p
 end
 
-#remote_file "/usr/src/php-5.5.2.tar.bz2" do
-#    source "http://us3.php.net/get/php-5.5.2.tar.bz2/from/us2.php.net/mirror"
-#    mode "0777"
-#end
-
-#execute "Expand PHP tarball" do
-#    cwd "/usr/src"
-#    user "root"
-#    command "tar -xvf php-5.5.2.tar.bz2"
-#end
-
-#execute "Configure PHP" do
-#    cwd "/usr/src/php-5.5.2"
-#    user "root"
-#    command "./configure --prefix=/usr --sysconfdir=/etc --with-config-file-path=/etc --enable-fpm --with-fpm-user=www-data --with-fpm-group=www-data --enable-opcache --enable-mbstring --enable-mbregex --enable-zip --with-mysqli --with-openssl --with-curl --with-zlib --enable-pcntl"
-#end
-
-#execute "Build & Install PHP" do
-#    cwd "/usr/src/php-5.5.2"
-#    user "root"
-#    command "make && make test && make install"
-#end
-
-cookbook_file "/usr/src/php_5.5.2-1_amd64.deb" do
-    source "php_5.5.2-1_amd64.deb"
-    mode "0755"
+cookbook_file "#{node[:php][:source_dir]}/#{node[:php][:deb_file]}" do
+    owner node[:php][:user]
+    group node[:php][:group]
+    source node[:php][:deb_file]
+    mode 00755
 end
 
 execute "Install PHP from Debian" do
-    cwd "/usr/src"
-    user "root"
-    command "dpkg -i php_5.5.2-1_amd64.deb"
+    cwd node[:php][:source_dir]
+    user node[:php][:user]
+    command "dpkg -i #{node[:php][:deb_file]}"
 end
 
-cookbook_file "/etc/php-fpm.conf" do
+cookbook_file "#{node[:php][:config_dir]}/php-fpm.conf" do
+    owner node[:php][:user]
+    group node[:php][:group]
     source "php-fpm.conf"
-    mode "0777"
+    mode 00755
 end
 
-cookbook_file "/etc/php.ini" do
+cookbook_file "#{node[:php][:config_dir]}/php.ini" do
+    owner node[:php][:user]
+    group node[:php][:group]
     source "xdebug-php.ini"
-    mode "0777"
+    mode 00755
 end
 
 cookbook_file "/etc/init.d/php-fpm" do
+    owner node[:php][:user]
+    owner node[:php][:group]
     source "init.d.php-fpm"
-    mode "0755"
+    mode 00755
 end
 
-execute "Enable PHP-FPM service" do
-    user "root"
-    command "update-rc.d php-fpm defaults"
+directory "#{node[:php][:log_dir]}/php-fpm" do
+    owner node[:php][:user]
+    group node[:php][:group]
 end
 
-execute "Create PHP log directories" do
-    user "root"
-    command "mkdir /var/log/php-fpm && mkdir /var/log/php"
+directory "#{node[:php][:log_dir]}/php" do
+    owner node[:php][:user]
+    group node[:php][:group]
 end
 
 execute "Install PECL_HTTP & XDebug" do
@@ -67,17 +51,12 @@ execute "Install PECL_HTTP & XDebug" do
     command "pecl update_channels && pecl install pecl_http xdebug"
 end
 
-# execute "Install PHPUnit" do
-#     user "root"
-#     command "pear upgrade pear && pear channel-discover pear.phpunit.de && pear channel-discover components.ez.no && pear channel-discover pear.symfony.com && pear install --alldeps phpunit/PHPUnit"
-# end
-
 remote_file "/usr/local/bin/composer" do
     source "https://getcomposer.org/composer.phar"
-    mode "0755"
+    mode 00755
 end
 
-execute "Start PHP-FPM Service" do
-    user "root"
-    command "/etc/init.d/php-fpm start"
+service "php-fpm" do
+    supports :status => true, :restart=>true, :reload=>true
+    action [:enable, :restart]
 end
